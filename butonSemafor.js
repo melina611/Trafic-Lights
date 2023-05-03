b70984fdcbab8b80652141fcf0c873a69b038539
melina611 <132465720+melina611@users.noreply.github.com>
2023-05-03 16:25:32 +0300

    Add files via upload

---
+++ butonSemafor.js
@@ -0,0 +1,11 @@
+
+function traficLightsChangeColors() {
+    let traficLights = document.getElementById("changeLightsColor").style.backgroundColor
+    if (traficLights === 'red') {
+        document.getElementById("changeLightsColor").style.backgroundColor = 'yellow'
+    } else if (traficLights === 'yellow') {
+        document.getElementById("changeLightsColor").style.backgroundColor = 'green'
+    } else {
+        document.getElementById("changeLightsColor").style.backgroundColor = 'red'
+    }
+}
